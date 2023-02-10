e9a82c41269728cd8642234fa8d06094a4aa3265
Samuel Kosasih <samadrianko@gmail.com>
2023-02-10 17:52:00 -0500

    Added tests for PacMan.consume()

--- Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
@@ -1,11 +1,21 @@
 package pacman;
 import java.io.*;
+
+import org.junit.Assert;
+
 import junit.framework.*;
 import java.awt.Color;
 
 public class TestPacManConsume extends TestCase {
 
   public void testPacManConsume() throws FileNotFoundException {
+    NoFrame frame = new NoFrame();
+    PacMan pacmanCookie = frame.addPacMan(new Location(2, 1));
+    PacMan pacmanNoCookie = frame.addPacMan(new Location(13, 9));
+
+    Assert.assertNull(pacmanCookie.consume());
+    Assert.assertNull(pacmanNoCookie.consume());
+
     return;
-  }
+  } 
 }
