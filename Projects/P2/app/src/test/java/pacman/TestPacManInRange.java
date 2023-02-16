80be10845a3d56f377372b87042202127ef4369f
Itai Iloni <itaiiloni@gmail.com>
2023-02-16 16:12:37 -0500

    is_ghost_in_range Test

--- Projects/P2/app/src/test/java/pacman/TestPacManInRange.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManInRange.java
@@ -1,11 +1,23 @@
 package pacman;
 import java.io.*;
 import junit.framework.*;
+import pacman.frames.NoFrame;
+
 import java.awt.Color;
 
 public class TestPacManInRange extends TestCase {
 
   public void testPacManInRange() throws FileNotFoundException {
-    return;
+    // Ghost in range
+    NoFrame frame = new NoFrame();
+    Ghost ghost = frame.addGhost(new Location(1,1), "Jared", Color.BLUE);
+    PacMan pacman = frame.addPacMan(new Location(2,1));
+    assertTrue(pacman.is_ghost_in_range());
+
+    // Ghost not in range
+    frame = new NoFrame(); // Clear map
+    ghost = frame.addGhost(new Location(4,1), "Jared", Color.BLUE);
+    pacman = frame.addPacMan(new Location(2,1));
+    assertFalse(pacman.is_ghost_in_range());
   }
 }
