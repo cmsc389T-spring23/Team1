1ec02864e9759daafef12b6cff3d019a00b6923f
Ayhan Eyikan <aeyikan@umd.edu>
2023-02-23 13:20:18 -0500

    implementation and test written

--- Projects/P2/app/src/main/java/pacman/Ghost.java
+++ Projects/P2/app/src/main/java/pacman/Ghost.java
@@ -14,8 +14,31 @@ public class Ghost {
   }
 
   public ArrayList<Location> get_valid_moves() {
-    // test
-    return null;
+    ArrayList<Location> moves = new ArrayList<>(4);
+
+    // check each direction : invalid if wall
+
+    // add north if safe
+    Location north = myLoc.shift(1, 0);
+    if (!myMap.getLoc(north).contains(Map.Type.WALL))
+      moves.add(north);
+
+    // add east if safe
+    Location east = myLoc.shift(0, 1);
+    if (!myMap.getLoc(east).contains(Map.Type.WALL))
+      moves.add(east);
+
+    // add south if safe
+    Location south = myLoc.shift(-1, 0);
+    if (!myMap.getLoc(south).contains(Map.Type.WALL))
+      moves.add(south);
+
+    // add west if safe
+    Location west = myLoc.shift(0, -1);
+    if (!myMap.getLoc(west).contains(Map.Type.WALL))
+      moves.add(west);
+
+    return moves;
   }
 
   public boolean move() {
--- Projects/P2/app/src/test/java/pacman/TestGhostValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostValidMoves.java
@@ -6,6 +6,25 @@ import java.awt.Color;
 public class TestGhostValidMoves extends TestCase {
 
   public void testGhostValidMoves() throws FileNotFoundException {
-    return;
+    // all valid moves
+    NoFrame frame = new NoFrame();
+
+    // ghost center, all empty around
+    Ghost ghost = frame.addGhost(new Location(12, 12), "Señor Spook", Color.BLUE);
+    ArrayList<Location> moves = ghost.get_valid_moves();
+    assertEquals(4, moves.size());
+    assertTrue(moves.contains(new Location(11, 12)));
+    assertTrue(moves.contains(new Location(13, 12)));
+    assertTrue(moves.contains(new Location(12, 11)));
+    assertTrue(moves.contains(new Location(12, 13)));
+
+    // ghost with a wall to the left and pacman
+    frame = new NoFrame();
+    Ghost ghost = frame.addGhost(new Location(11, 11), "Monsieur Mole", Color.GREEN);
+    PacMan pacman = frame.addPacMan(new Location(11, 12));
+    assertEquals(3, moves.size());
+    assertTrue(moves.contains(new Location(11, 12)));
+    assertTrue(moves.contains(new Location(11, 10)));
+    assertTrue(moves.contains(new Location(12, 11)));
   }
 }
