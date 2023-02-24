9c03d016c820740ffe03ed2c34ee63acb946d42d
iiloni-umd <iiloni@terpmail.umd.edu>
2023-02-24 16:52:39 -0500

    Added null checking

--- Projects/P2/app/src/main/java/pacman/PacMan.java
+++ Projects/P2/app/src/main/java/pacman/PacMan.java
@@ -27,7 +27,7 @@ public class PacMan {
     // Check all 8 adjacent tiles for a ghost
     for (int x = myLoc.x - 1; x <= myLoc.x + 1; x++) {
       for (int y = myLoc.y - 1; y <= myLoc.y + 1; y++)
-        if (myMap.getLoc(new Location(x, y)).contains(Map.Type.GHOST)) {
+        if (myMap.getLoc(new Location(x, y)) != null && myMap.getLoc(new Location(x, y)).contains(Map.Type.GHOST)) {
           return true;
         }
     }
