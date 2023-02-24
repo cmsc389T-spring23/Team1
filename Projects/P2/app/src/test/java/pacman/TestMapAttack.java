b4736cb9fbf9bb46174fff50e803b057e893f7c9
iiloni-umd <iiloni@terpmail.umd.edu>
2023-02-24 17:20:12 -0500

    Initial Map-attack commit

--- Projects/P2/app/src/test/java/pacman/TestMapAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestMapAttack.java
@@ -6,6 +6,18 @@ import java.awt.Color;
 public class TestMapAttack extends TestCase {
 
   public void testMapAttack() throws FileNotFoundException {
-    return;
+    // Ghost in range
+    NoFrame frame = new NoFrame();
+    Ghost ghost = frame.addGhost(new Location(1,1), "Jared", Color.BLUE);
+    PacMan pacman = frame.addPacMan(new Location(2,1));
+    assertTrue(ghost.attack());
+    assertTrue(frame.getMap().isGameOver());
+
+    // Ghost not in range
+    frame = new NoFrame(); // Clear map
+    ghost = frame.addGhost(new Location(4,1), "Jared", Color.BLUE);
+    pacman = frame.addPacMan(new Location(2,1));
+    assertFalse(ghost.attack());
+    assertFalse(frame.getMap().isGameOver());
   }
 }
