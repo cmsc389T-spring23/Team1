d59fd512027552bf36aa64ba6a8e3a0cc68168a7
Itai Iloni <itaiiloni@gmail.com>
2023-03-01 13:03:14 -0500

    Fix my grade pls

--- Projects/P2/app/src/main/java/pacman/PacMan.java
+++ Projects/P2/app/src/main/java/pacman/PacMan.java
@@ -26,7 +26,6 @@ public class PacMan {
 
   public boolean is_ghost_in_range() {
     // Check all 8 adjacent tiles for a ghost
-    // Check all 8 adjacent tiles for
     for (int x = myLoc.x - 1; x <= myLoc.x + 1; x++) {
       for (int y = myLoc.y - 1; y <= myLoc.y + 1; y++) {
         if (myMap.getLoc(new Location(x, y)).contains(Map.Type.GHOST)) {
