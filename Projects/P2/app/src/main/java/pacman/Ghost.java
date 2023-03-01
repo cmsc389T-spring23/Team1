d11a3acd1e247bddedd831135113c28952877c34
nathanb9 <nahuk9@gmail.com>
2023-03-01 13:43:31 -0500

    remove accidental changes

--- Projects/P2/app/src/main/java/pacman/Ghost.java
+++ Projects/P2/app/src/main/java/pacman/Ghost.java
@@ -18,15 +18,7 @@ public class Ghost {
   }
 
   public boolean move() {
-    ArrayList<Location> valid_moves = get_valid_moves();
-    if (valid_moves == null || valid_moves.size() == 0) 
-      return false;
-    else {
-      Location new_loc = valid_moves.get(0);
-      myMap.move(myName, new_loc, Map.Type.GHOST);
-      return true;
-    }
-    
+    return false;
   }
 
   public boolean is_pacman_in_range() {
