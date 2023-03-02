651abf77325211cc6c11e1f294cd5ba292c21d32
Ayhan Eyikan <aeyikan@umd.edu>
2023-03-01 23:05:53 -0500

    fix pacman test Location coords and add new case

--- Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
@@ -16,25 +16,39 @@ public class TestPacManValidMoves extends TestCase {
     ArrayList<Location> moves = pacman.get_valid_moves();
 
     assertEquals(4, moves.size());
-    assertTrue(moves.contains(new Location(11, 12)));
-    assertTrue(moves.contains(new Location(13, 12)));
-    assertTrue(moves.contains(new Location(12, 11)));
-    assertTrue(moves.contains(new Location(12, 13)));
+    assertTrue(moves.contains(new Location(11, 12)));  // above
+    assertTrue(moves.contains(new Location(13, 12)));  // below
+    assertTrue(moves.contains(new Location(12, 11)));  // left
+    assertTrue(moves.contains(new Location(12, 13)));  // right
 
     // pacman with a wall to the left and ghosts
     frame = new NoFrame();
-    pacman = frame.addPacMan(new Location(11, 11));
-    Ghost ghost = frame.addGhost(new Location(11, 10), "Sir Noodle", Color.BLUE);
-    ghost = frame.addGhost(new Location(11, 12), "Sir Noodle", Color.BLUE);
-    ghost = frame.addGhost(new Location(12, 11), "Sir Noodle", Color.BLUE);
+    pacman = frame.addPacMan(new Location(12, 11));
+
+    Ghost ghost = frame.addGhost(new Location(11, 11), "Sir Noodle", Color.BLUE);  // above
+    ghost = frame.addGhost(new Location(12, 12), "Sir Noodle", Color.BLUE);  // right
+    ghost = frame.addGhost(new Location(13, 11), "Sir Noodle", Color.BLUE);  // below
 
     moves = pacman.get_valid_moves();
     
     assertEquals(3, moves.size());
-    assertTrue(moves.contains(new Location(11, 12)));
-    assertTrue(moves.contains(new Location(11, 10)));
-    assertTrue(moves.contains(new Location(12, 11)));
+    assertTrue(moves.contains(new Location(11, 11)));  // above
+    assertTrue(moves.contains(new Location(13, 11)));  // below
+    assertTrue(moves.contains(new Location(12, 12)));  // right
 
-    return;
+
+    // pacman with cookie to right
+    frame = new NoFrame();
+    pacman = frame.addPacMan(new Location(12, 13)); 
+
+    ghost = frame.addGhost(new Location(12, 12), "Gronk", Color.PINK);  // left
+
+    moves = pacman.get_valid_moves();
+
+    assertEquals(4, moves.size());
+    assertTrue(moves.contains(new Location(12, 14)));  // right
+    assertTrue(moves.contains(new Location(12, 12)));  // left
+    assertTrue(moves.contains(new Location(11, 13)));  // above
+    assertTrue(moves.contains(new Location(13, 13)));  // below
   }
 }
