898d0626ce5b6009127959abc58fb2c080b6016a
Samuel Kosasih <samadrianko@gmail.com>
2023-03-01 22:40:52 -0500

    added tests as some went missing

--- Projects/P2/app/src/test/java/pacman/TestMapGetLoc.java
+++ Projects/P2/app/src/test/java/pacman/TestMapGetLoc.java
@@ -6,6 +6,15 @@ import java.awt.Color;
 public class TestMapGetLoc extends TestCase {
 
   public void testMapGetLoc() throws FileNotFoundException {
+    Map map = new Map(5);
+    map.add("Pacman1", new Location(1,1), null, Map.Type.PACMAN);
+    map.add("Cookie1", new Location(2,2), null, Map.Type.COOKIE);
+
+    assertTrue(map.getLoc(new Location(1,1)).contains(Map.Type.PACMAN));
+    assertTrue(map.getLoc(new Location(2,2)).contains(Map.Type.COOKIE));
+    assertTrue(map.getLoc(new Location(3,3)).contains(Map.Type.EMPTY));
+    assertTrue(map.getLoc(new Location(6,6)).contains(Map.Type.WALL));
+    
     return;
   }
 }
--- Projects/P2/app/src/test/java/pacman/TestPacManInRange.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManInRange.java
@@ -6,6 +6,17 @@ import java.awt.Color;
 public class TestPacManInRange extends TestCase {
 
   public void testPacManInRange() throws FileNotFoundException {
+    // Ghost in range
+    NoFrame frame = new NoFrame();
+    Ghost ghost = frame.addGhost(new Location(1,1), "Jared", Color.BLUE);
+    PacMan pacman = frame.addPacMan(new Location(2,1));
+    assertTrue(pacman.is_ghost_in_range());
+    // Ghost not in range
+    frame = new NoFrame(); // Clear map
+    ghost = frame.addGhost(new Location(4,1), "Jared", Color.BLUE);
+    pacman = frame.addPacMan(new Location(2,1));
+    assertFalse(pacman.is_ghost_in_range());
+    
     return;
   }
 }
